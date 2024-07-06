e6758890920e5d2503854395896b8b3344744ab9
pelumini <pelumini@gmail.com>
2024-07-06 09:29:51 +0100

    added create course module

--- src/app/(dashboard)/(routes)/teacher/courses/page.tsx
+++ src/app/(dashboard)/(routes)/teacher/courses/page.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { DataTable } from "./_components/data-table";
+import { columns } from "./_components/columns";
+import prisma from "@/lib/prisma";
 
-const CoursesPage = () => {
-  return <div>CoursesPage</div>;
+const CoursesPage = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    return redirect("/");
+  }
+
+  const courses = await prisma.course.findMany({
+    where: {
+      userId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  return (
+    <div className="p-6">
+      <DataTable columns={columns} data={courses} />
+    </div>
+  );
 };
 
 export default CoursesPage;
--- src/app/(dashboard)/_components/Logo.tsx
+++ src/app/(dashboard)/_components/Logo.tsx
@@ -1,10 +1,5 @@
 import Image from "next/image";
 
 export const Logo = () => {
-  return (
-    <div className="flex items-center gap-1">
-      <Image height={50} width={50} alt="logo" src="/logo.svg" />
-      <h1 className="font-bold text-xl">xKnow</h1>
-    </div>
-  );
+  return <Image height={130} width={130} alt="logo" src="/logo.svg" />;
 };
